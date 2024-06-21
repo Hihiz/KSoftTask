0b7ecbb63881a8511f42b6b1fa3f589a802889dd
Hihiz <public@mail.ru>
2024-06-21 23:11:06 +0300

    Добавить сущнось книги

---
+++ KSoftTask.Domain/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace KSoftTask.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<AuthorBook> AuthorBooks { get; }
+        public ICollection<PublisherBook> PublisherBooks { get; }
+    }
+}
