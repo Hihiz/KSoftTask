588284cfaf76100f48883649cee04bb757d467f9
Hihiz <public@mail.ru>
2024-06-21 23:10:53 +0300

    Добавить сущнось автора

---
+++ KSoftTask.Domain/Entities/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace KSoftTask.Domain.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<AuthorBook> AuthorBooks { get; }
+    }
+}
