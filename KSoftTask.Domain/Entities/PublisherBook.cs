c2cf04224edf633bfba542bd7c5e99ad2e4cb75e
Hihiz <public@mail.ru>
2024-06-21 23:14:18 +0300

    Добавить сущнось издатели с книгами

---
+++ KSoftTask.Domain/Entities/PublisherBook.cs
@@ -0,0 +1,12 @@
+﻿namespace KSoftTask.Domain.Entities
+{
+    public class PublisherBook
+    {
+        public int Id { get; set; }
+        public int PublisherId { get; set; }
+        public Publisher? Publisher { get; set; }
+
+        public int BookId { get; set; }
+        public Book? Book { get; set; }
+    }
+}
