971385ffd0bab32b5e962eccb36899d0f139ccdd
Hihiz <public@mail.ru>
2024-06-22 23:32:42 +0300

    Добавить интерфейс контекста

---
+++ KSoftTask.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using KSoftTask.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KSoftTask.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Book> Books { get; set; }
+        DbSet<Author> Authors { get; set; }
+        DbSet<Publisher> Publishers { get; set; }
+        DbSet<AuthorBook> AuthorBooks { get; set; }
+        DbSet<PublisherBook> PublisherBooks { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
