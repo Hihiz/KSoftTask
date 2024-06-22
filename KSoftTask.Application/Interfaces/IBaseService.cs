7a1b7841d99160d8ec2110a5dd7229113f517513
Hihiz <public@mail.ru>
2024-06-22 23:33:10 +0300

    Добавить базовый сервис

---
+++ KSoftTask.Application/Interfaces/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace KSoftTask.Application.Interfaces
+{
+    public interface IBaseService<T, CreateDto, UpdateDto>
+    {
+        Task<List<T>> GetAll(CancellationToken cancellationToken = default);
+        Task<T> GetById(int id, CancellationToken cancellationToken = default);
+        Task<T> Create(CreateDto dto, CancellationToken cancellationToken = default);
+        Task<T> Update(int id, UpdateDto dto, CancellationToken cancellationToken = default);
+        Task Delete(int id, CancellationToken cancellationToken = default);
+    }
+}
