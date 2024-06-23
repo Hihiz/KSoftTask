12b16c198b7ca24004c2b00605d682467b5ab4a9
Hihiz <public@mail.ru>
2024-06-24 00:49:38 +0300

    Добавить класс контекста данных

---
+++ KSoftTask.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using KSoftTask.Application.Interfaces;
+using KSoftTask.Domain.Entities;
+using KSoftTask.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KSoftTask.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<AuthorBook> AuthorBooks { get; set; }
+        public DbSet<PublisherBook> PublisherBooks { get; set; }
+    }
+}
