796e156d53e407020c32f24ce6f4d5f87a32dad8
Hihiz <public@mail.ru>
2024-06-23 23:12:16 +0300

    Добавить валидацию для издателя

---
+++ KSoftTask.Application/Validations/FluentValidations/Publishers/CreatePublisherValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using KSoftTask.Application.Dto.Publishers;
+
+namespace KSoftTask.Application.Validations.FluentValidations.Publishers
+{
+    public class CreatePublisherValidator : AbstractValidator<CreatePublisherDto>
+    {
+        public CreatePublisherValidator()
+        {
+            RuleFor(x => x.Title)
+                  .NotEmpty().WithMessage("Введите название издательства")
+                  .MinimumLength(3).WithMessage("Название издательства короткое");
+        }
+    }
+}
---
+++ KSoftTask.Application/Validations/FluentValidations/Publishers/UpdatePublisherValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using KSoftTask.Application.Dto.Publishers;
+
+namespace KSoftTask.Application.Validations.FluentValidations.Publishers
+{
+    public class UpdatePublisherValidator : AbstractValidator<UpdatePublisherDto>
+    {
+        public UpdatePublisherValidator()
+        {
+            RuleFor(x => x.Id)
+                 .NotEmpty().WithMessage("Укажите номер издательства");
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Введите название издательства")
+                .MinimumLength(3).WithMessage("Название издательства короткое");
+        }
+    }
+}
