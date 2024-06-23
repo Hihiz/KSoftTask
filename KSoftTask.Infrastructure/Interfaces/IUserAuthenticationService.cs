eb94f7345ceef3fd3e1fec804bfdb128bbfa8ae7
Hihiz <public@mail.ru>
2024-06-24 00:52:28 +0300

    Добавить интерфейс для аутентификации

---
+++ KSoftTask.Infrastructure/Interfaces/IUserAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using KSoftTask.Infrastructure.Models;
+
+namespace KSoftTask.Infrastructure.Interfaces
+{
+    public interface IUserAuthenticationService
+    {
+        Task<TokenModel> LoginAsync(LoginModel model);
+        Task<RegistrationModel> RegisterAsync(RegistrationModel model);
+        Task LogoutAsync();
+    }
+}
