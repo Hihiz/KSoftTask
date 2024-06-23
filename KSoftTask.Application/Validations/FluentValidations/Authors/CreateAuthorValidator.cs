8acd003e3233db4466d44fe368ef41dc1cf2bd1d
Hihiz <public@mail.ru>
2024-06-23 23:14:09 +0300

    Добавить валидацию для автора

---
+++ KSoftTask.Application/Validations/FluentValidations/Authors/CreateAuthorValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using KSoftTask.Application.Dto.Authors;
+
+namespace KSoftTask.Application.Validations.FluentValidations.Authors
+{
+    public class CreateAuthorValidator : AbstractValidator<CreateAuthorDto>
+    {
+        public CreateAuthorValidator()
+        {
+            RuleFor(x => x.Name)
+               .NotEmpty().WithMessage("Введите автора книги")
+               .MinimumLength(3).WithMessage("Имя автора короткое !");
+        }
+    }
+}
---
+++ KSoftTask.Application/Validations/FluentValidations/Authors/UpdateAuthorValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using KSoftTask.Application.Dto.Authors;
+
+namespace KSoftTask.Application.Validations.FluentValidations.Authors
+{
+    public class UpdateAuthorValidator : AbstractValidator<UpdateAuthorDto>
+    {
+        public UpdateAuthorValidator()
+        {
+            RuleFor(x => x.Id)
+                 .NotEmpty().WithMessage("Укажите номер автора !");
+            RuleFor(x => x.Name)
+             .NotEmpty().WithMessage("Введите автора книги")
+             .MinimumLength(3).WithMessage("Имя автора короткое !");
+        }
+    }
+}
