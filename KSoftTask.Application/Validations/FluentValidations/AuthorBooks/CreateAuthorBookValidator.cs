c57e7e0bdbf704d23ae3825979a9c55c4e05d269
Hihiz <public@mail.ru>
2024-06-23 23:13:11 +0300

    Добавить валидацию для автора и книг

---
+++ KSoftTask.Application/Validations/FluentValidations/AuthorBooks/CreateAuthorBookValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using KSoftTask.Application.Dto.AuthorBooks;
+
+namespace KSoftTask.Application.Validations.FluentValidations.AuthorBooks
+{
+    public class CreateAuthorBookValidator : AbstractValidator<CreateAuthorBookDto>
+    {
+        public CreateAuthorBookValidator()
+        {
+            RuleFor(x => x.AuthorId)
+                .NotEmpty().WithMessage("Укажите автора книги")
+                 .GreaterThan(0).WithMessage("Выберите автора книги!");
+            RuleFor(x => x.BookId)
+                .NotEmpty().WithMessage("Укажите книгу")
+                 .GreaterThan(0).WithMessage("Выберите книгу !");
+        }
+    }
+}
---
+++ KSoftTask.Application/Validations/FluentValidations/AuthorBooks/UpdateAuthorBookValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using KSoftTask.Application.Dto.AuthorBooks;
+
+namespace KSoftTask.Application.Validations.FluentValidations.AuthorBooks
+{
+    public class UpdateAuthorBookValidator : AbstractValidator<UpdateAuthorBookDto>
+    {
+        public UpdateAuthorBookValidator()
+        {
+            RuleFor(x => x.Id)
+                 .NotEmpty().WithMessage("Укажите номер автора книг");
+            RuleFor(x => x.AuthorId)
+              .NotEmpty().WithMessage("Укажите автора книги")
+               .GreaterThan(0).WithMessage("Выберите автора книги!");
+            RuleFor(x => x.BookId)
+                .NotEmpty().WithMessage("Укажите книгу")
+                 .GreaterThan(0).WithMessage("Выберите книгу !");
+        }
+    }
+}
