fee9d683b28e5272987fae0385360ab639d5f978
Hihiz <public@mail.ru>
2024-06-24 00:52:02 +0300

    Добавить интерфейс токена

---
+++ KSoftTask.Infrastructure/Interfaces/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using KSoftTask.Infrastructure.Models;
+
+namespace KSoftTask.Infrastructure.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<TokenModel> RefreshToken(TokenModel token);
+        Task<string> RevokeUser(string username);
+        Task<string> RevokeAll();
+    }
+}
