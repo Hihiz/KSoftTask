c905ff50319d5bb6eca7c86263358305f460f2e4
Hihiz <public@mail.ru>
2024-06-23 23:13:55 +0300

    Добавить валидацию для книг

---
+++ KSoftTask.Application/Validations/FluentValidations/Books/CreateBookValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using KSoftTask.Application.Dto.Books;
+
+namespace KSoftTask.Application.Validations.FluentValidations.Books
+{
+    public class CreateBookValidator : AbstractValidator<CreateBookDto>
+    {
+        public CreateBookValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Введите название книги")
+                .MinimumLength(3).WithMessage("Название книги короткое");
+        }
+    }
+}
---
+++ KSoftTask.Application/Validations/FluentValidations/Books/UpdateBookValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using KSoftTask.Application.Dto.Books;
+
+namespace KSoftTask.Application.Validations.FluentValidations.Books
+{
+    public class UpdateBookValidator : AbstractValidator<UpdateBookDto>
+    {
+        public UpdateBookValidator()
+        {
+            RuleFor(x => x.Id)
+                 .NotEmpty().WithMessage("Укажите номер книги");
+            RuleFor(x => x.Title)
+                 .NotEmpty().WithMessage("Введите название книги")
+                 .MinimumLength(3).WithMessage("Название книги короткое");
+        }
+    }
+}
